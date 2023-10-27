773b3dc95888593186758b211f1a8e971971aebf
zombieteer <aryan.nigam1996@gmail.com>
2023-10-27 19:45:06 +0530

    host changed

--- scripts/insertIndividuals.js
+++ scripts/insertIndividuals.js
@@ -1,12 +1,11 @@
 const fs = require("fs");
 const csv = require("csvtojson");
-const db = require("../utils/db");
 const geoJSONtoWKT = require("./convert");
 const { Pool } = require("pg");
 
 const pool = new Pool({
   user: "bsa",
-  host: "127.0.0.1",
+  host: "db",
   database: "bsadb",
   password: "bsa",
   port: 5432,
